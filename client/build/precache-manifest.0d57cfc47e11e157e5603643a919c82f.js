766db72cf2b2b9cf1217e87afa7b2a4d64c18fc1
Nongnooch Roongpiboonsopit <nongnoochr@gmail.com>
2020-01-07 23:22:31 +0700

    Add build folder

---
+++ client/build/precache-manifest.0d57cfc47e11e157e5603643a919c82f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d751c09db99a06385a44346fd5efa67",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28107415317b1158abbf",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "26b447225dc709402a56",
+    "url": "/static/css/main.57ab031f.chunk.css"
+  },
+  {
+    "revision": "28107415317b1158abbf",
+    "url": "/static/js/2.4b63df4f.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/static/js/2.4b63df4f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "26b447225dc709402a56",
+    "url": "/static/js/main.1942995b.chunk.js"
+  },
+  {
+    "revision": "6b3defdcd169e5f902d5",
+    "url": "/static/js/runtime-main.1dc66141.js"
+  }
+]);
